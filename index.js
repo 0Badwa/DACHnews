d5ca3a2c9470b77d374bbfd1511a6191dcb2fa99
0Badwa <ontarioont55@gmail.com>
2025-01-17 20:15:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ async function sendBatchToGPT(feedBatch) {
     id: item.id,
     title: item.title,
     description: item.description
+    console.log(`Naslov: "${title}", Opis: "${description}", Rezultat: ${result}`);
+
   }));
 
   const payload = {
