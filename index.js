526ec61a6156449cc34f36d71e3e3319adf87ab2
0Badwa <ontarioont55@gmail.com>
2025-02-09 07:52:47 +0100

    ubaceni fontovi lokalno2

--- index.js
+++ index.js
@@ -23,6 +23,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+// Služenje statičkih fajlova iz "fonts/" foldera
+app.use('/fonts', express.static(path.join(__dirname, 'src/fonts')));
+
 app.use(
   helmet({
     contentSecurityPolicy: false,
